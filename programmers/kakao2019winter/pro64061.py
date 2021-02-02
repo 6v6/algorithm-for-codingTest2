a5418fe5333fa938c2f311650a641a424014d9dd
6v6 <injeong410@gmail.com>
2021-02-03 00:28:54 +0900

    [pro] 64061+크리인인형뽑기게임

--- programmers/kakao2019winter/pro64061.py
+++ programmers/kakao2019winter/pro64061.py
@@ -1,1 +1,20 @@
 #크레인 인형뽑기 게임
+def solution(board, moves):
+    answer = 0
+    stack = []
+    n = len(board[0])
+    for i in moves:
+        for j in range(n):
+            if board[j][i-1] != 0:
+                if stack:
+                    if stack[-1] != board[j][i-1]:
+                        stack.append(board[j][i-1])
+                    else:
+                        answer += 2
+                        stack.pop()
+                else:
+                    stack.append(board[j][i-1])
+                board[j][i-1] = 0
+                break
+            
+    return answer
