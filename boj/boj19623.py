762de07b12296b8c02ad561457f310a532f1ba38
6v6 <injeong410@gmail.com>
2021-02-08 17:11:17 +0900

    시간초과 ㅜㅠ

--- boj/boj19623.py
+++ boj/boj19623.py
@@ -2,34 +2,42 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def dfs(data, i, dp, numbers):
-    global max
-    if dp[i] < numbers:
-        dp[i] = numbers
-    else:
-        return
-    endTime = data[i][1]
-
-    for j in range(i+1, len(data)):
-        nStartTime = data[j][0]
-        if  endTime <= nStartTime:
-            newNum = numbers + data[j][2]
-            dfs(data, j, dp, newNum)
-
-
 n = int(input())
 data = []
-dp = [0] * n
+times = []
 
 for i in range(n):
-    s, e, num  = list(map(int, input().split()))
-    data.append([s,e,num])
-data = sorted(data)
+    data.append(list(map(int, input().split())))
+    times.append(data[i][0])
+    times.append(data[i][1])
+
+#좌표압축
+data.sort()
+times = sorted(set(times))
+dic = {value : index for index, value in enumerate(times)}
+
+for i in range(len(data)):
+    data[i][0] = dic[data[i][0]]
+    data[i][1] = dic[data[i][1]]
+
+dp = [0] * len(times)
+
+for dt in data:
+    if dp[dt[1]] < dt[2]:
+        dp[dt[1]] = dt[2]
+
 #print(data)
-
-for i in range(n):
-    dfs(data, i, dp, data[i][2])
-
+for i in range(len(data)):
+    if dp[data[i][1]] > data[i][2]:
+        continue 
+    endTime = data[i][1]
+    for j in range(i+1,len(data)):
+        nStartTime = data[j][0]
+        if  endTime <= nStartTime and dp[data[j][1]] < dp[endTime] + data[j][2]:
+            dp[data[j][1]] = dp[endTime] + data[j][2]
+           # print(endTime," ",nStartTime," ",dp[data[j][1]])
+            endTime = data[j][1]
+#print(dp)
 max = sorted(dp)[-1]
 print(max)
 
